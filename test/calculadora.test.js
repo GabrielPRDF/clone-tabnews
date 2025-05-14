7417cd189a1600dde183ab6367665c816a70088a
Gabriel Fonseca <gabriel-prdf@hotmail.com>
2025-05-14 14:10:46 -0300

    Creat one test with jest

---
+++ test/calculadora.test.js
@@ -0,0 +1,16 @@
+// Exemplos
+// test("nome do teste", callbackFunction);
+// function callbackFunction() {
+//   console.log("Esta função foi executada");
+// }
+//
+// test("nome do teste 2", function () {
+//   console.log("Esta função foi executada 2");
+// });
+//
+// test("nome do teste 2", () => {
+//   console.log("Esta função foi executada 3");
+// });
+test("espero que 1 seja 1", () => {
+  expect(1).toBe(1);
+});

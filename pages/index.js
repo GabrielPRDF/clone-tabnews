1cb009b4870a2303d4d8e632cd95c09f641e596b
Gabriel Fonseca <gabriel-prdf@hotmail.com>
2025-05-17 10:24:36 -0300

    Creat page loading

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,52 @@
 function Home() {
   return (
-    <h1>
-      Faça seu melhor com as condições que tem, até conseguir fazer melhor com
-      condições melhores!
-    </h1>
+    <div>
+      <div className="container">
+        <div className="container">
+          <div className="flex">
+            <div>Página em construção</div>
+            <div className="loading-container">
+              <div className="loading"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <style jsx>{`
+        .container {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          height: 90vh;
+          margin: 0;
+        }
+
+        .loading-container {
+          position: relative;
+          width: 50px;
+          height: 50px;
+          margin: auto;
+        }
+
+        .loading {
+          position: absolute;
+          width: 100%;
+          height: 100%;
+          border: 4px solid #3498db;
+          border-radius: 50%;
+          border-top-color: transparent;
+          animation: spin 1s linear infinite;
+        }
+
+        @keyframes spin {
+          0% {
+            transform: rotate(0deg);
+          }
+          100% {
+            transform: rotate(360deg);
+          }
+        }
+      `}</style>
+    </div>
   );
 }
 

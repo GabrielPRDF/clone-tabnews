a29a0f1b85c0673309b36a8bffbc0c185b307692
Gabriel Fonseca <gabriel-prdf@hotmail.com>
2025-05-19 19:30:38 -0300

    start configuration postgresql

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,9 @@
-function index(request, response) {
+import database from "../../../../infra/database";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result);
   response.status(200).json({ chave: "Estamos progredindo \u{1F600}" });
 }
 
-export default index;
+export default status;

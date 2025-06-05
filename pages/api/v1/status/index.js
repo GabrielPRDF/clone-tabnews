17f29e11fd87be9b1cff8b6095bffa0e7adde1d8
Gabriel Fonseca <gabriel-prdf@hotmail.com>
2025-06-05 18:12:49 -0300

    Adjust db

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -7,12 +7,19 @@ async function status(request, response) {
   const activityConnections = await database.query(
     "SELECT COUNT(state) FROM pg_stat_activity WHERE state = 'active'",
   );
-  console.log(versionDB);
+  // const databaseOpenedConnectionsResult = await database.query(
+  //   "SELECT count(*)::int FROM pg_stat_activity WHERE datname = 'local_db'",
+  // );
+
+  // console.log(databaseOpenedConnectionsResult.rows[0].count);
+  // const activityConnections = await database.query(
+  //   "SELECT COUNT(state) FROM pg_stat_database WHERE state = 'active'",
+  // );
   response.status(200).json({
     updated_at: updatedAt,
     version_db: versionDB.rows[0].server_version,
-    max_connection: maxConnection.rows[0].max_connections,
-    activity_connection: activityConnections.rows[0].count,
+    max_connection: parseInt(maxConnection.rows[0].max_connections),
+    activity_connection: parseInt(activityConnections.rows[0].count),
   });
 }
 
--- test/integration/api/v1/status/get.test.ts
+++ test/integration/api/v1/status/get.test.ts
@@ -8,17 +8,13 @@ test("GET to /api/v1/status should return 200", async () => {
   console.log(responseBody);
   // @ts-ignore
   expect(responseBody.updated_at).toBeDefined();
-
   const parseUpdatedAt = new Date(responseBody.updated_at).toISOString();
   // @ts-ignore
   expect(responseBody.updated_at).toEqual(parseUpdatedAt);
-
   // @ts-ignore
   expect(responseBody.version_db).toBe("16.9");
-
   // @ts-ignore
-  expect(Number(responseBody.max_connection)).toBe(100);
-
+  expect(responseBody.max_connection).toBe(100);
   // @ts-ignore
-  expect(Number(responseBody.activity_connection)).toBe(1);
+  expect(responseBody.activity_connection).toBe(1);
 });
